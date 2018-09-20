2798c15d8100fca448b6773e8a42fbb04a5054be
Pinger <wanitme@gmail.com>
2018-09-20 12:45:32 +0800

    添加base基类快速开发库

--- app/src/main/java/com/thunder/player/MainActivity.kt
+++ app/src/main/java/com/thunder/player/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.thunder.player
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import com.fungo.baselib.base.basic.BaseActivity
 import com.fungo.funplayer.controller.StandardPlayerController
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity(override val layoutResID: Int = R.layout.activity_main) : BaseActivity() {
 
     private var mUrl = "http://221.228.226.5/14/z/w/y/y/zwyyobhyqvmwslabxyoaixvyubmekc/sh.yinyuetai.com/4599015ED06F94848EBF877EAAE13886.mp4"
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+    override fun initData() {
         val controller = StandardPlayerController(this)
 
         controller.setTitle("这是点播")
         funPlayer.setController(controller)
         funPlayer.setUrl(mUrl)
         funPlayer.start()
+
+        setSwipeBackEnable(false)
+    }
+
+    override fun onDestroy() {
+        funPlayer?.release()
+        super.onDestroy()
+    }
+
+    override fun onStop() {
+        funPlayer?.stopPlayback()
+        super.onStop()
     }
 }
---
+++ app/src/main/java/com/thunder/player/app/PlayerApplication.kt
@@ -0,0 +1,10 @@
+package com.thunder.player.app
+
+import com.fungo.baselib.app.BaseApplication
+
+/**
+ * @author Pinger
+ * @since 2018/9/20 下午12:33
+ */
+class PlayerApplication : BaseApplication() {
+}
